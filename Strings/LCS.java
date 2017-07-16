53d799009945ee8062684120be5b3af44b082b42
Razvan <r77feed@gmail.com>
2017-07-17 01:33:16 +0300

    Fixed LCS

--- Strings/LCS.java
+++ Strings/LCS.java
@@ -2,11 +2,14 @@ public class LCS{
 	
 	public void len(char[] a, char[] b){
 		int matrix[][] = new int[a.length+1][b.length+1];
+
 		for(int i=1;i<=a.length;i++){
 			for(int j=1;j<=b.length;j++){
-				if (a[i-1]==b[j-1]) 
+				if (a[i-1]==b[j-1]) {
 					matrix[i][j]=matrix[i-1][j-1]+1;
-					//matrix[i][j]=Math.max(matrix[i][j-1],matrix[i-1][j]);
+					}
+				else
+					matrix[i][j]=Math.max(matrix[i][j-1],matrix[i-1][j]);
 				}
 
 		}
@@ -24,6 +27,8 @@ public class LCS{
 	public static void main(String[] args){
 		String a="RRRASDASDASCDAS";
 		String b="SADASCASDCASCRRR";
+
+
 		new LCS().len(a.toCharArray(), b.toCharArray());
 	}
 }
