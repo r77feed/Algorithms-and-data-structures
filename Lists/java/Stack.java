6004618ac798e617fbe321675342a8539d0d8d27
Razvan <r77feed@gmail.com>
2017-07-30 15:03:36 +0300

    Add Stack class

---
+++ Lists/java/Stack.java
@@ -0,0 +1,51 @@
+public class Stack<T> {
+
+	public SimpleNode<T> root;
+
+	public T pop(){
+		SimpleNode<T> curNode = new SimpleNode<T>();
+		T value = root.data;
+		curNode = root;
+		root = root.next;
+		curNode = null;	
+		return value;
+		
+	}
+	public T peek(){
+		return root.data;
+	}
+	public int push(T value){
+		if (root==null){
+			root = new SimpleNode<T>();
+			root.data = value;
+		}
+		else{
+			SimpleNode<T> newNode = new SimpleNode<T>();
+			newNode.data = value;
+			newNode.next = root;
+			root = newNode;
+		}
+		return 0;
+	}
+
+	public void printAll(){
+		if (root!=null){
+			SimpleNode<T> curNode  = new SimpleNode<T>();
+			curNode = root;
+			while(curNode != null){
+				System.out.println(pop()+"! ");
+				curNode = curNode.next;
+			}
+		}
+	}
+
+	public static void main(String[] args){
+
+		int a[] = {1,2,3,4,5,12};
+		Stack<Integer> s = new Stack<Integer>();
+		for (int x :a){
+			s.push(x);
+		}
+		s.printAll();
+	}
+}
